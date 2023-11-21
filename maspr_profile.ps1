94c60e95964c56728a0290ee6c4864dee7af541d
Pratama Nur Wijaya <set.mnemonix@gmail.com>
2023-11-21 19:08:17 +0700

    Update maspr_profile.ps1

--- maspr_profile.ps1
+++ maspr_profile.ps1
@@ -1,13 +1,4 @@
 oh-my-posh init pwsh --config 'C:\Users\maspr\AppData\Local\Programs\oh-my-posh\themes\easy-term.omp.json' | Invoke-Expression
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
 
 set-alias -name ll -value ls
 set-alias -name gw -value ./gradlew
@@ -24,3 +15,5 @@ function java17 {
   $Env:Path="$Env:JAVA_HOME\bin;$Env:Path"
   set "Path=%JAVA_HOME%\bin;%Path%"
 }
+
+Invoke-Expression (&starship init powershell)
