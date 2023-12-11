e906dfae897571cb70268ad6a204126ed74e6a38
pratamawijaya <set.mnemonix@gmail.com>
2023-12-11 22:16:01 +0700

    update lua
    update lua config nvim

---
+++ config/nvim/lua/user/keymaps.lua
@@ -0,0 +1,14 @@
+local keymap = vim.keymap -- for conciseness
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+
+-- window movement
+keymap.set("n", "sd", "<C-w>h", { desc="move to window left"})
+keymap.set("n", "st", "<C-w>k", { desc="move to window top"})
+keymap.set("n", "sh", "<C-w>j", { desc="move to window bottom"})
+keymap.set("n", "sn", "<C-w>l", { desc="move to window right"})
